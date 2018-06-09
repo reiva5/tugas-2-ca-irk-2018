fc118f96629418628e949ac5b9e6ed48f14ced60
reiva5 <jehiannormansaviero@gmail.com>
2018-06-10 00:36:12 +0700

    Add Compressed.pas for Pascal

--- pascal/Compressed.pas
+++ pascal/Compressed.pas
@@ -1,16 +1,29 @@
-unit compress;
+unit Compressed;
 
 interface
 
 type
-	Compressed = class
-		published
+	Compress = object
+		public
 			function toBytes(): PChar;
 			{ANOTHER METHOD OR VARIABLE NEEDED}
 	end;
 
+function doCompress(namaFile: string) : Compress;
+procedure decompress(namaFile : string);
+
 implementation
-function Compressed.toBytes(): PChar;
+function Compress.toBytes(): PChar;
+begin
+
+end;
+
+function doCompress(namaFile: string) : Compress;
+begin
+
+end;
+
+procedure decompress(namaFile : string);
 begin
 
 end;
