84e49c8100808d15926c5f52a2ec93f050bf089a
reiva5 <jehiannormansaviero@gmail.com>
2018-06-10 01:06:30 +0700

    Add implementation file Compressed.cpp for C++

---
+++ cpp/Compressed.cpp
@@ -0,0 +1,15 @@
+#include "Compressed.h"
+
+unsigned char* Compress::toBytes(){
+	unsigned char* str;
+	return str;
+}
+
+Compress doCompress(char* namaFile){
+	Compress compress;
+	return compress;
+}
+
+void decompress(char* namaFile){
+	return;
+}
