d87c85fd7607029c31e7d826b001c1f82ed46a47
reiva5 <jehiannormansaviero@gmail.com>
2018-06-10 00:55:00 +0700

    Add compressed.c

---
+++ c/Compressed.c
@@ -0,0 +1,14 @@
+#include "Compressed.h"
+
+unsigned char* toBytes(Compress* compress){
+	return "";
+}
+
+Compress doCompress(char* namaFile){
+	Compress compress;
+	return compress;
+}
+
+void decompress(char* namaFile){
+	return;
+}
