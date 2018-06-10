d2b48cb6ce32e0a5df22543c2b889cee32fbb4a6
reiva5 <jehiannormansaviero@gmail.com>
2018-06-10 16:57:49 +0700

    Add compressed.py in Python

--- python/Compressed.py
+++ python/Compressed.py
@@ -1,3 +1,12 @@
 class Compressed:
-	def toBytes(self)
+	def __init__(self):
+		print("done")
+	def toBytes(self):
+		return "me"
 	# YOU CAN ADD ANOTHER METHOD OR VARIABLE HERET
+
+def doCompress(namaFile):
+	return "pass"
+
+def decompress(namaFile):
+	return "suasu"
