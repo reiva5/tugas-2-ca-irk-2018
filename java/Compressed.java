29067a8c64c881ab364ce6830f1e64143dca4347
Pollycarpus <fijarmayoza.rabbi@gmail.com>
2018-06-30 13:19:03 +0700

    add huffman

--- java/Compressed.java
+++ java/Compressed.java
@@ -1,6 +1,15 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Compressed {
 	private String asu = "Asu";
